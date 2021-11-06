e17b95c60c1c30a334fd98adca1211f7016b192f
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-11-06 23:42:01 +0530

    UPDATE: chart aspect ratio updated

--- src/components/charts/Chart.jsx
+++ src/components/charts/Chart.jsx
@@ -6,7 +6,7 @@ const SalesChart = ({ data, dataKeys, title, color }) => {
 	return (
 		<div className="sales-chart">
 			<h3 className="sales-chart__title">{title}</h3>
-			<ResponsiveContainer width="100%" aspect={4 / 1}>
+			<ResponsiveContainer width="100%" aspect={3 / 1}>
 				<LineChart data={data}>
 					<XAxis dataKey={dataKeys[0]} stroke="#c6c6c6" fontSize={15} />
 					<Line type="monotone" dataKey={dataKeys[1]} stroke={color} strokeWidth={2} />
